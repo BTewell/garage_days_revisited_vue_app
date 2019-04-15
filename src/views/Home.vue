f8c53a621366373df12ec3938ae74f2245d5db42
“Brett <bretttewell@gmail.com>
2019-04-15 17:51:21 -0500

    initial commit, add bootstrap, create index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="event in events">
+      <h2>{{ event.title }}</h2>
+      <img v-bind:src="event.images">
+      <p>{{event.details}}</p>
+      <p>{{event.address}}</p>
+      <p>{{event.start_date}}</p>
+      <p>{{event.end_date}}</p>
+      <p>{{event.start_time}} - {{event.end_time}}</p>
+
+    </div>
   </div>
 </template>
 
+<style>
+img{
+  width: 200px;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Garage Days Revisited",
+      events: [],
+    };
+  },
+  created: function() {
+    axios.get('/api/events').then(response => {
+      this.events = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
